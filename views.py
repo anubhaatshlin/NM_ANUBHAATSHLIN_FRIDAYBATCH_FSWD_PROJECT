ee5072e426bb9b0acd87bb9cb18548330cc0b8e2
anubhaatshlin <145701413+anubhaatshlin@users.noreply.github.com>
2024-04-10 21:03:22 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,47 +1,83 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .models import Contact
-# from django.core.mail import send_mail
-# from django.contrib.auth.models import User
-
+from django.shortcuts import render, get_object_or_404
+from .models import Car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
-def inquiry(request):
-    if request.method == 'POST':
-        car_id = request.POST['car_id']
-        car_title = request.POST['car_title']
-        user_id = request.POST['user_id']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        customer_need = request.POST['customer_need']
-        city = request.POST['city']
-        state = request.POST['state']
-        email = request.POST['email']
-        phone = request.POST['phone']
-        message = request.POST['message']
+def cars(request):
+    cars = Car.objects.order_by('-created_date')
+    paginator = Paginator(cars, 4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
+    data = {
+        'cars': paged_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+    }
+    return render(request, 'cars/cars.html', data)
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, 'cars/car_detail.html', data)
 
 
-        # if request.user.is_authenticated:
-        #     user_id = request.user.id
-        #     has_contacted = Contact.objects.all().filter(car_id=car_id, user_id=user_id)
-        #     if has_contacted:
-        #         messages.error(request, 'You have already made an inquiry about this car. Please wait until we get back to you.')
-        #         return redirect('/cars/'+car_id)
+def search(request):
+    cars = Car.objects.order_by('-created_date')
 
-        contact = Contact(car_id=car_id, car_title=car_title, user_id=user_id,
-        first_name=first_name, last_name=last_name, customer_need=customer_need, city=city,
-        state=state, email=email, phone=phone, message=message)
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission', flat=True).distinct()
 
-        # admin_info = User.objects.get(is_superuser=True)
-        # admin_email = admin_info.email
-        # send_mail(
-        #         'New Car Inquiry',
-        #         'You have a new inquiry for the car ' + car_title + '. Please login to your admin panel for more info.',
-        #         'harribdhr@gmail.com',
-        #         [admin_email],
-        #         fail_silently=False,
-        #     )
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
 
-        contact.save()
-        messages.success(request, 'Your request has been submitted, we will get back to you shortly.')
-        return redirect('/cars/'+car_id)
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(city__iexact=city)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
+    data = {
+        'cars': cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
+    }
+    return render(request, 'cars/search.html', data)
